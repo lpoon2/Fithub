6bdcd51252a89f1c9125dea4e67310a784d34216
Larry Poon <lpoon2@illinois.du>
2016-11-22 14:20:35 -0600

    added passport

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ var app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var session      = require('express-session');
+
 app.use(express.static(__dirname + '/public'));
 
+// required for passport
+app.use(session({
+  secret: 'cs498finalProject',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session()); // persistent login sessions
+ app.use(flash());
 var port = process.env.PORT || 3000;
 mongoose.connect("mongodb://fithub:cs498fa2016@ds161487.mlab.com:61487/fithub");
 console.log("Express server running on " + port);
