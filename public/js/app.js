bb0cab57f7a373bfceea7b6f2a749352985139f4
Arjun Menon <apmenon2@illinois.edu>
2016-11-29 06:01:11 +0000

    homepage setup

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+var app=angular.module("fithub",["ngRoute","fithubControllers"]);app.config(function($routeProvider){$routeProvider.when("/home",{templateUrl:"partials/home.html",controller:"homeControl"}).otherwise({redirectTo:"/home"})});
---
+++ public/js/controllers.js
@@ -0,0 +1,1 @@
+var fithubControllers=angular.module("fithubControllers",[]);fithubControllers.controller("homeControl",["$scope",function($scope){$scope.data="",$scope.displayText="",$scope.setData=function(){CommonData.setData($scope.data),$scope.displayText="Data set"}}]);
