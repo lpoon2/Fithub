a26030abbda33050030ba0851459e7f4c6c6be2e
mahersamawi <mahersamawi@github.com>
2016-11-29 06:27:59 +0000

    modified element schema

--- models/element.js
+++ models/element.js
@@ -6,6 +6,11 @@ var eleSchema   = new mongoose.Schema({
   type : String,
   media : String,  // the path to the media ?
   keywords: [String], // array of tags that pertain to the workout
+  repetitions: int,
+  sets: int,
+  // if this a cardio lift (ie running, biking, swimming)
+  duration: int,
+  pace: int
 });
 
 module.exports = mongoose.model('Element', eleSchema);
