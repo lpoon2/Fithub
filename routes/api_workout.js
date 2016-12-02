3bf01c6e2c5c36f95f319bb7a1775893d7b468b0
xcchangxc@163.com <xuchangxc@didichuxing.com>
2016-12-02 17:34:59 -0600

    workout api

--- routes/api_workout.js
+++ routes/api_workout.js
@@ -2,6 +2,18 @@ var work = require('../models/workout.js');
 
 module.exports = function(router) {
 
+  function ErrMsg(error){
+    var msg = "Error: ";
+    if (!error.errors){
+      msg += error.message;
+    }else{
+      //var space = true;
+      for (var key in error.errors){
+        msg += error.errors[key].message + ' ';
+      }
+    }
+    return msg;
+  }
 
 router.get('/workout', function(req,res){
 work.find(function(err, obj){
@@ -18,21 +30,21 @@ router.post('/workout',function(req,res){
   var ele = new work();
   ele.name = req.body.name;
   ele.description = req.body.description;
-  ele.original_user = req.body.original_user;
-  ele.current_user = req.body.current_user;
+  ele.original_user = req.body.original_user;//*original user need to be changed to null if created or previous user
+  ele.current_user = req.body.current_user;//current user need to be changed in front end
   ele.rating = req.body.rating;
   ele.comments = req.body.comments;
   ele.keywords = req.body.keywords;
   ele.elements = req.body.elements;
-  ele.day.Mon = req.body.day.Mon;
+  ele.day.Mon = req.body.day.Mon;//is this enough to make a deep copy ?
   ele.day.Tue = req.body.day.Tue;
   ele.day.Wed = req.body.day.Wed;
   ele.day.Thu = req.body.day.Thu;
   ele.day.Fri = req.body.day.Fri;
-  ele.save(function(err, obj){
+  ele.save(function(err){
     if(err){
       res.status(500);
-      res.json(err);
+      res.json({message: ErrMsg(error)});
     }
     else{
       res.status(201);
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,5 @@ module.exports = function (app, router) {
   app.use('/api', require('./api_element.js')(router));
   app.use('/api', require('./api_workout.js')(router));
   app.use('/api', require('./login.js')(router));
-
+  app.use('/api', require('./api_workoutid')(router))
 };
